e22a34eba54d2ad4a73c81dd4f50e31073a9a03e
pino._.hoo <inhoo23@naver.com>
2022-09-25 18:56:12 +0900

    add : / 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import Home from './components/home/home'
 import Login from './components/login/login'
 import Register from './components/register/register'
 
@@ -6,6 +7,7 @@ export default function App() {
   return (
     <div>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/auth/login" element={<Login />} />
         <Route path="/auth/register" element={<Register />} />
       </Routes>
