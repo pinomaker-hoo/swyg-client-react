547b53c1892acb9a66c1e3f0a84eaf8f141a4feb
pino._.hoo <inhoo23@naver.com>
2022-10-15 22:26:50 +0900

    fix : book Page

--- src/pages/book/book.tsx
+++ src/pages/book/book.tsx
@@ -1,12 +1,106 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import { getBook } from "../../api/book"
+import Header from "../../components/Header"
+
+import {
+  BodyBox,
+  BookBox,
+  BookBtn,
+  BookImage,
+  BookInfo,
+  BookLeftBox,
+  BookRightBox,
+  BookStory,
+  BookSub,
+  BookTextBox,
+  BookTitle,
+  BottomBox,
+  BottomTitle,
+  CommentBox,
+  CommentIcon,
+  CommentInfo,
+  CommentLeft,
+  CommentName,
+  CommentRight,
+  CommentText,
+  ImageBox,
+  Title,
+  TopBox,
+} from "./style"
 import { InBox, OuterBox } from "./style"
 
 export default function Book() {
+  const [book, setBook]: any = useState()
   const { id } = useParams()
-  console.log(id)
+
+  useEffect(() => {
+    callApi()
+  }, [])
+
+  const callApi = async () => {
+    if (id) {
+      const { data } = await getBook(id)
+      setBook(() => data)
+    }
+  }
+  console.log(book)
+  if (!book) return null
   return (
     <OuterBox>
-      <InBox></InBox>
+      <InBox>
+        <Header />
+        <BodyBox>
+          <TopBox>
+            <Title>키 쑥쑥 영양책</Title>
+            <BookBox>
+              <BookLeftBox>
+                <BookImage src={book.thumbnail} />
+              </BookLeftBox>
+              <BookRightBox>
+                <BookTextBox>
+                  <BookTitle>{book.title}</BookTitle>
+                  <BookInfo>
+                    작가 : {book.authors}, 출판사 : {book.publisher}
+                  </BookInfo>
+                  <BookSub>줄거리</BookSub>
+                  <BookStory>{book.contents}</BookStory>
+                  <BookBtn color="#F18B45">퀴즈 맞추기</BookBtn>
+                  <BookBtn color="#805FC7">퀴즈 내기</BookBtn>
+                </BookTextBox>
+              </BookRightBox>
+            </BookBox>
+          </TopBox>
+          <BottomBox>
+            <BottomTitle>친구들이 단 코멘트 </BottomTitle>
+            <Comment />
+          </BottomBox>
+        </BodyBox>
+      </InBox>
     </OuterBox>
+  )
+}
+
+const Comment = () => {
+  return (
+    <CommentBox>
+      <CommentLeft>
+        <ImageBox src="../../../public/profile3.jpeg" />
+      </CommentLeft>
+      <CommentRight>
+        <CommentName>김인후</CommentName>
+        <CommentText>
+          비밀교실은 원하는 것을 상상하면 뭐든 이루어지는 곳이다. 부모님이
+          이혼하셔서 아빠와 살고 있는 한이는 엄마와 만나거 싶다고 생각했다.
+          그래서 비밀교실에서 엄마를 만나게 됐다. 한이가 엄마를 만나는
+          장면에서는 나도 눈물이 날 것 같았다. 시우는 놀이동산처럼 재미있는
+          학교를 상상...
+        </CommentText>
+        <CommentIcon src="../../../public/icon-like.png" />
+        <CommentInfo>5</CommentInfo>
+        <CommentIcon src="../../../public/icon-comment.png" />
+        <CommentInfo>10</CommentInfo>
+      </CommentRight>
+    </CommentBox>
   )
 }
--- src/pages/book/style.ts
+++ src/pages/book/style.ts
@@ -4,11 +4,122 @@ export const OuterBox = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 672px;
-  height: 972px;
+  width: 1100px;
 `
 
 export const InBox = styled.div`
   width: 100%;
   height: 70%;
+`
+export const BodyBox = styled.div``
+
+export const TopBox = styled.div`
+  margin-top: 100px;
+  width: 1100px;
+  height: 750px;
+`
+export const BottomBox = styled.div`
+  background-color: white;
+  width: 1100px;
+  height: 800px;
+`
+
+export const Title = styled.h1`
+  color: white;
+`
+
+export const BookBox = styled.div``
+export const BookRightBox = styled.div`
+  width: 60%;
+  height: 100%;
+  float: left;
+  margin-left: 5%;
+`
+export const BookLeftBox = styled.div`
+  width: 35%;
+  height: 100%;
+  float: left;
+`
+export const BookImage = styled.img`
+  width: 100%;
+`
+export const BookTextBox = styled.div`
+  margin-left: 50px;
+`
+export const BookTitle = styled.h1`
+  color: white;
+`
+export const BookInfo = styled.p`
+  color: white;
+  font-size: 20px;
+`
+export const BookSub = styled.h3`
+  margin-top: 80px;
+  color: white;
+`
+export const BookStory = styled.p`
+  color: white;
+  font-size: 20px;
+  line-height: 30px;
+`
+
+export const BookBtn = styled.button`
+  width: 160px;
+  height: 54px;
+  border-radius: 27px;
+  background-color: ${(props) => props.color};
+  color: white;
+  font-size: 20px;
+  margin-right: 24px;
+  margin-top: 50px;
+`
+
+export const BottomTitle = styled.h1`
+  color: black;
+  margin-bottom: 70px;
+`
+export const CommentBox = styled.div`
+  width: 1100px;
+  height: 300px;
+  border-radius: 20px;
+  box-shadow: 0px 0px 5px 3px #efefef;
+`
+
+export const CommentLeft = styled.div`
+  width: 10%;
+  height: 100%;
+  float: left;
+`
+export const CommentRight = styled.div`
+  width: 90%;
+  height: 100%;
+  float: left;
+`
+
+export const ImageBox = styled.img`
+  margin-top: 20px;
+  width: 84px;
+  height: 84px;
+  border-radius: 42px;
+`
+export const CommentName = styled.h1`
+  margin-top: 20px;
+`
+export const CommentText = styled.p`
+  font-size: 20px;
+  line-height: 30px;
+`
+export const CommentIcon = styled.img`
+  width: 35px;
+  height: 35px;
+  margin-top: 20px;
+  float: left;
+`
+
+export const CommentInfo = styled.h3`
+  color: #f18b45;
+  float: left;
+  margin-left: 20px;
+  margin-right: 20px;
+  font-size: 28px;
 `
