216195cc3c79bb2b32bf76e66fbb852df0bfb55d
pino._.hoo <inhoo23@naver.com>
2022-10-25 02:25:02 +0900

    add : like Book api

---
+++ src/api/likeBook.ts
@@ -0,0 +1,40 @@
+import axios from "axios"
+
+const likeBook = axios.create({
+  baseURL: "http://localhost:8003",
+  withCredentials: true,
+  headers: {},
+})
+
+export const saveLikeBook = async (bookId: string) => {
+  try {
+    return await likeBook({
+      url: `/${bookId}`,
+      method: "post",
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export const deleteLikeBook = async (bookId: string) => {
+  try {
+    return await likeBook({
+      url: `/${bookId}`,
+      method: "delete",
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export const getLikeBookList = async () => {
+  try {
+    return await likeBook({
+      url: "/",
+      method: "get",
+    })
+  } catch (err) {
+    console.log(err)
+  }
+}
