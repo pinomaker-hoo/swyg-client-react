7ef8e69e4167685edd7816d32960ef4ecf31e3c1
pino._.hoo <inhoo23@naver.com>
2022-10-25 00:49:14 +0900

    add : parents page

--- src/pages/parents/parents.tsx
+++ src/pages/parents/parents.tsx
@@ -1,7 +1,112 @@
+import Header from "../../components/Header"
+import {
+  BodyBox,
+  BookBox,
+  BookImage,
+  BookImageTop,
+  DateBox,
+  DateColum,
+  DateText,
+  DateTitle,
+  FirstBox,
+  FourthBox,
+  FourthTitle,
+  ImgBox,
+  InBox,
+  ListLi,
+  ListText,
+  ListUl,
+  NameText,
+  OuterBox,
+  SecondBox,
+  TagBtn,
+  ThirdBox,
+  ThirdTitle,
+} from "./style"
+
 export default function Parents() {
   return (
-    <div>
-      <h1>Parents</h1>
-    </div>
+    <OuterBox>
+      <InBox>
+        <Header />
+        <BodyBox>
+          <FirstBox>
+            <ImgBox src="../../../public/profile3.jpeg" />
+            <NameText>김인후</NameText>
+          </FirstBox>
+          <SecondBox>
+            <DateTitle>최근 독서 기록</DateTitle>
+            <DateBox>
+              <DateColum>
+                <DateText>일</DateText>
+                <DateText>0</DateText>
+              </DateColum>
+              <DateColum>
+                <DateText>월</DateText>
+                <DateText>0</DateText>
+              </DateColum>
+              <DateColum>
+                <DateText>화</DateText>
+                <DateText>0</DateText>
+              </DateColum>
+              <DateColum>
+                <DateText>수</DateText>
+                <DateText>0</DateText>
+              </DateColum>
+              <DateColum>
+                <DateText>목</DateText>
+                <DateText>0</DateText>
+              </DateColum>
+              <DateColum>
+                <DateText>금</DateText>
+                <DateText>0</DateText>
+              </DateColum>
+              <DateColum>
+                <DateText>토</DateText>
+                <DateText>0</DateText>
+              </DateColum>
+            </DateBox>
+          </SecondBox>
+          <ThirdBox>
+            <ThirdTitle>이번 주 독서 평가</ThirdTitle>
+            <ListUl>
+              <ListLi>과학에 관심이 많고 과학책을 가장 많이 읽었어요.</ListLi>
+              <ListLi>책을 읽고 다른 친구가 낸 퀴즈를 모두 맞추었어요.</ListLi>
+              <ListLi>퀴즈를 내는 문장 어휘력이 조금 부족해 보여요.</ListLi>
+              <ListLi>
+                다른 친구의 코멘트에 긍정적인 댓글을 달고 소통했어요.
+              </ListLi>
+            </ListUl>
+            <ListText>보호자 TIP!</ListText>
+            <ListUl>
+              <ListLi>
+                과학이 다른 도서 분야와도 상관관계가 있다는 것을 알려주세요!
+              </ListLi>
+              <ListLi>
+                아이가 과학에 흥미를 잃지 않도록 집에서 할 수 있는 과학 실험을
+                보호자께서 지도해주세요!
+              </ListLi>
+            </ListUl>
+          </ThirdBox>
+          <FourthBox>
+            <FourthTitle>평가 기반 추천 도서</FourthTitle>
+            <TagBtn>#언어자극</TagBtn>
+            <BookBox>
+              <BookImageTop />
+              <BookImage />
+              <BookImage />
+              <BookImage />
+            </BookBox>
+            <TagBtn>#어휘력 향상</TagBtn>
+            <BookBox>
+              <BookImageTop />
+              <BookImage />
+              <BookImage />
+              <BookImage />
+            </BookBox>
+          </FourthBox>
+        </BodyBox>
+      </InBox>
+    </OuterBox>
   )
 }
--- src/pages/parents/style.ts
+++ src/pages/parents/style.ts
@@ -1,14 +1,107 @@
 import styled from "styled-components"
+
 export const OuterBox = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 1300px;
-  height: 972px;
+  width: 1100px;
 `
 
 export const InBox = styled.div`
   width: 100%;
   height: 70%;
+`
+export const BodyBox = styled.div``
+
+export const FirstBox = styled.div`
+  margin-top: 100px;
+  width: 1100px;
+  height: 400px;
+  justify-content: center;
+  align-items: center;
+`
+export const SecondBox = styled.div`
+  margin-top: 100px;
+  width: 1100px;
+  height: 400px;
+`
+export const ThirdBox = styled.div`
+  margin-top: 100px;
+  width: 1100px;
+  height: 750px;
+`
+export const FourthBox = styled.div`
+  margin-top: 100px;
+  width: 1100px;
+  height: 750px;
+`
+export const ImgBox = styled.img`
+  width: 275px;
+  height: 275px;
+  border: 3px white solid;
+  border-radius: 137.5px;
+`
+export const NameText = styled.h1`
+  color: white;
+`
+export const DateBox = styled.div`
+  width: 100%;
+  height: 250px;
+  background-color: white;
+`
+export const DateTitle = styled.h3`
+  color: white;
+`
+export const DateColum = styled.div`
+  width: ${100 / 7}%;
+  height: 100%;
+  float: left;
+  text-align: center;
+`
+export const DateText = styled.h3`
+  color: black;
+  font-size: 32px;
+  margin-top: 50px;
+`
+export const ThirdTitle = styled.h1`
+  color: white;
+`
+
+export const ListUl = styled.ul``
+export const ListLi = styled.li`
+  color: white;
+  margin-bottom: 30px;
+`
+export const ListText = styled.h3`
+  color: #f18b45;
+`
+
+export const TagBtn = styled.button`
+  background-color: white;
+  width: 151px;
+  height: 54px;
+  border-radius: 27px;
+  color: #442d7a;
+  margin-top: 50px;
+`
+export const FourthTitle = styled.h1`
+  color: white;
+`
+export const BookBox = styled.div`
+  width: 100%;
+  height: 250px;
+  margin-top: 30px;
+`
+export const BookImageTop = styled.img`
+  width: 230px;
+  height: 340px;
+  float: left;
+  text-align: center;
+`
+export const BookImage = styled.img`
+  width: 230px;
+  height: 340px;
+  margin-left: 50px;
+  float: left;
   text-align: center;
 `
