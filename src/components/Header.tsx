7429c3a52a09a65e97aa928cfe31ce963abbcfa5
pino._.hoo <inhoo23@naver.com>
2022-10-26 11:35:12 +0900

    fix : Header about logined

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,31 @@
-import { Link } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
 import styled from "styled-components"
+import { getCookie, removeCookie } from "../common/Cookie"
 
 export default function Header() {
+  const [logined, setLogined] = useState(false)
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    checkLogined()
+  }, [])
+
+  const checkLogined = async () => {
+    const cookie = await getJwtCookie()
+    cookie ? setLogined(() => true) : setLogined(() => false)
+  }
+
+  const getJwtCookie = async (): Promise<string> => {
+    return await getCookie("accessToken")
+  }
+
+  const logoutBtn = async () => {
+    removeCookie("accessToken")
+    navigate("/")
+  }
+
   return (
     <OuterBox>
       <Link to={"/"}>
@@ -10,8 +34,16 @@ export default function Header() {
       <Link to={"/likebook"}>
         <MenuText>찜</MenuText>
       </Link>
-      <MenuText>로그아웃</MenuText>
-      <MenuText>보호자</MenuText>
+      {logined ? (
+        <MenuText onClick={logoutBtn}>로그아웃</MenuText>
+      ) : (
+        <Link to={"/auth/login"}>
+          <MenuText>로그인</MenuText>
+        </Link>
+      )}
+      <Link to={"/parents"}>
+        <MenuText>보호자</MenuText>
+      </Link>
     </OuterBox>
   )
 }
