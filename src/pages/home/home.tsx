727ce16cea1aa0ff5fbd87a5be9db78b01cef84f
pino._.hoo <inhoo23@naver.com>
2022-10-27 10:17:39 +0900

    style : home style

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,8 +1,39 @@
 import { useState } from "react"
-import { Link } from "react-router-dom"
 import { KakaoSearch, SaveBook } from "../../api/book"
 import Header from "../../components/Header"
-import { BodyBox, InBox, OuterBox } from "./styles"
+import { BookTitle } from "../book/style"
+import {
+  BodyBox,
+  FifthBox,
+  FirstBox,
+  FirstImg,
+  FirstLeftBox,
+  FirstRightBox,
+  FirstTitleText,
+  FourthBody,
+  FourthBox,
+  FourthImgCard,
+  FourthImgCardImg,
+  FourthImgCardLeft,
+  FourthImgCardRight,
+  FourthImgCardText,
+  FourthImgCardTitle,
+  FourthTitle,
+  InBox,
+  OuterBox,
+  PText,
+  SecondBox,
+  SecondInput,
+  SecondTagBtn,
+  SecondTagBtnBox,
+  SecondTitle,
+  ThirdBookCard,
+  ThirdBookCardBox,
+  ThirdBookCardImg,
+  ThirdBookCardText,
+  ThirdBox,
+  ThirdSubTitle,
+} from "./styles"
 
 export default function Home() {
   const [text, setText] = useState("")
@@ -35,35 +66,77 @@ export default function Home() {
       <InBox>
         <Header />
         <BodyBox>
-          <Link to={"/auth/login"}>
-            <button>LOGIN</button>
-          </Link>
-          <Link to={"/auth/register"}>
-            <button>REGISTER</button>
-          </Link>
-          <Link to={"/quiz/27"}>
-            <button>QUIZ</button>
-          </Link>
-          <Link to={"/auth/info"}>
-            <button>INFO</button>
-          </Link>
-          <Link to={"/quiz/make/27"}>
-            <button>make</button>
-          </Link>
-          <Link to={"/quiz/make/choice/27"}>
-            <button>Quiz Choice</button>
-          </Link>
-          <Link to={"/book/27"}>
-            <button>Book</button>
-          </Link>
-          <Link to={"/parents"}>
-            <button>Parents</button>
-          </Link>
-          <br />
-          <input onChange={onChange} />
-          <button onClick={onClick}>CLICK</button>
+          <FirstBox>
+            <FirstLeftBox>
+              <PText>00아 북도리를 만난 걸 환영해</PText>
+              <FirstTitleText>바깥 세상이 궁금해!</FirstTitleText>
+            </FirstLeftBox>
+            <FirstRightBox>
+              <FirstImg src="../../../public/egg.png" />
+            </FirstRightBox>
+          </FirstBox>
+          <SecondBox>
+            <SecondTitle>어떤 책을 찾고 있어?</SecondTitle>
+            <SecondInput placeholder="ex. 안나의 일기" />
+            <SecondTagBtnBox>
+              <SecondTagBtn>#흥미진진한</SecondTagBtn>
+              <SecondTagBtn>#모험적인</SecondTagBtn>
+              <SecondTagBtn>#인기있는</SecondTagBtn>
+              <SecondTagBtn>#스릴있는</SecondTagBtn>
+              <SecondTagBtn>#신기한</SecondTagBtn>
+            </SecondTagBtnBox>
+          </SecondBox>
+          <ThirdBox>
+            <BookTitle>북도리 성장을 도와주는 책</BookTitle>
+            <ThirdSubTitle>
+              관심 있는 카테고리 책을 골라 <br />
+              메이트 성장을 도울 수 있어요!
+            </ThirdSubTitle>
+            <ThirdBookCardBox>
+              <ThirdBook />
+              <ThirdBook />
+              <ThirdBook />
+              <ThirdBook />
+              <ThirdBook />
+            </ThirdBookCardBox>
+          </ThirdBox>
+          <FourthBox>
+            <FourthTitle>친구들이 가장 많이 읽은 Top 6</FourthTitle>
+            <FourthBody>
+              <FourthBook />
+              <FourthBook />
+              <FourthBook />
+              <FourthBook />
+              <FourthBook />
+              <FourthBook />
+            </FourthBody>
+          </FourthBox>
+          <FifthBox></FifthBox>
         </BodyBox>
       </InBox>
     </OuterBox>
+  )
+}
+
+const ThirdBook = (props: any) => {
+  return (
+    <ThirdBookCard>
+      <ThirdBookCardImg />
+      <ThirdBookCardText>비밀교실</ThirdBookCardText>
+    </ThirdBookCard>
+  )
+}
+
+const FourthBook = () => {
+  return (
+    <FourthImgCard>
+      <FourthImgCardLeft>
+        <FourthImgCardImg />
+      </FourthImgCardLeft>
+      <FourthImgCardRight>
+        <FourthImgCardTitle>가짜 모범생</FourthImgCardTitle>
+        <FourthImgCardText>#재미있는</FourthImgCardText>
+      </FourthImgCardRight>
+    </FourthImgCard>
   )
 }
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -12,3 +12,107 @@ export const InBox = styled.div`
   height: 70%;
 `
 export const BodyBox = styled.div``
+
+export const FirstBox = styled.div`
+  width: 100%;
+  height: 400px;
+  background-color: blue;
+`
+export const SecondBox = styled.div`
+  width: 100%;
+  height: 300px;
+  background-color: red;
+  margin-top: 100px;
+`
+export const ThirdBox = styled.div`
+  width: 100%;
+  height: 500px;
+  background-color: blue;
+  margin-top: 100px;
+`
+export const FourthBox = styled.div`
+  width: 100%;
+  height: 500px;
+  background-color: red;
+  margin-top: 100px;
+`
+export const FifthBox = styled.div`
+  width: 100%;
+  height: 300px;
+  background-color: blue;
+  margin-top: 100px;
+`
+export const FirstLeftBox = styled.div`
+  width: 50%;
+  height: 100%;
+  float: left;
+`
+
+export const FirstRightBox = styled.div`
+  width: 50%;
+  height: 100%;
+  float: left;
+`
+export const PText = styled.p`
+  color: white;
+`
+
+export const FirstTitleText = styled.h1``
+
+export const FirstImg = styled.img`
+  width: 275.4px;
+  height: 380px;
+`
+export const SecondTitle = styled.h1``
+
+export const SecondInput = styled.input`
+  width: 80%;
+  height: 100px;
+`
+
+export const SecondTagBtnBox = styled.div``
+export const SecondTagBtn = styled.button`
+  width: 160px;
+  height: 50px;
+  border: none;
+  border-radius: 25px;
+  background-color: white;
+  border: 3px solid #ffef62;
+  float: left;
+  margin-right: 30px;
+`
+
+export const ThirdTitle = styled.h1``
+export const ThirdSubTitle = styled.h3``
+export const ThirdBookCardBox = styled.div``
+export const ThirdBookCard = styled.div`
+  float: left;
+  width: 180px;
+  height: 300px;
+  background-color: green;
+  margin-right: 30px;
+`
+export const ThirdBookCardImg = styled.img``
+export const ThirdBookCardText = styled.h3``
+
+export const FourthTitle = styled.h1``
+export const FourthBody = styled.div``
+export const FourthImgCard = styled.div`
+  width: 300px;
+  height: 200px;
+  background-color: green;
+  float: left;
+`
+export const FourthImgCardImg = styled.img``
+export const FourthImgCardTitle = styled.p``
+export const FourthImgCardText = styled.h3``
+export const FourthImgCardLeft = styled.div`
+  float: left;
+  width: 50%;
+  height: 100%;
+`
+export const FourthImgCardRight = styled.div`
+  float: left;
+  width: 50%;
+  height: 100%;
+`
