712c4ae15673cb13a7fa492759582be0a98282e9
pino._.hoo <inhoo23@naver.com>
2022-11-06 10:38:42 +0900

    Update parents.tsx

--- src/pages/parents/parents.tsx
+++ src/pages/parents/parents.tsx
@@ -80,6 +80,14 @@ export default function Parents() {
     if (data) location.href = "/parents"
   }
 
+  const isSameDate = (date1: Date, date2: Date) => {
+    return (
+      date1.getFullYear() === date2.getFullYear() &&
+      date1.getMonth() === date2.getMonth() &&
+      date1.getDate() === date2.getDate()
+    )
+  }
+
   const getDateToDay = (data: any) => {
     const date = new Date(data)
     const WEEKDAY: number[] = [0, 1, 2, 3, 4, 5, 6]
